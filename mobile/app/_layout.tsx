495b365c758aa87ff7ef18691bd2d1f9f7748558
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-03-29 21:42:34 -0300

    feat: basic auth screen

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,7 +1,25 @@
+import { Stack } from "expo-router";
 
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from '../lib/supabase'
+import Auth from '../components/Auth'
+import { View, Text } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
 export default function RootLayout() {
-    }
+	const [session, setSession] = useState<Session | null>(null);
 
+	useEffect(() => {
+		supabase.auth.getSession().then(({ data: { session } }) => {
+			setSession(session)
+		})
+		supabase.auth.onAuthStateChange((_event, session) => {
+			setSession(session)
+		})
+	}, []);
 
+	return <View>
+		{ (session && session.user) ? <Stack/> : <Auth/> }
+	</View>;
 }
