5bd6a6b3988ccd1cd65a19dbb5ae84734d460ced
Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
2025-04-03 14:28:48 -0300

    quick fix

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,54 +1,26 @@
-import { SplashScreen, Stack } from "expo-router";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Slot } from "expo-router";
+import NavigationBar from "@/components/NavigationBar";
+import TopBar from "@/components/TopBar";
 
-import 'react-native-url-polyfill/auto'
-import { useState, useEffect } from 'react'
-import { supabase } from '../lib/supabase'
-import Auth from '../components/Auth'
-import { AuthChangeEvent, Session } from '@supabase/supabase-js'
-import { ThemeProvider } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-	const [ session, setSession ] = useState<Session | null>(null);
-
-	// This loads instantly, but is set up so it can await fonts or other critical resources.
-	const [loaded, setLoaded] = useState(true);
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
-		}
-	}, [loaded]);
-	
-	if (!loaded) {
-		return null;
-	}
-
-	useEffect(() => {
-		supabase.auth.getSession().then(({ data: { session } }) => {
-			setSession(session);
-		});
-
-		supabase.auth.onAuthStateChange((_event, session) => {
-			setSession(session);
-		});
-	}, []);
-
-	return (
-		// <ThemeProvider > */}
-		(session && session.user )
-		? 
-			<>
-			<Stack>
-				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-				<Stack.Screen name="+not-found" />
-			</Stack>
-			<StatusBar style="auto" />
-			</>
-		:
-			<Auth/>
-		// </ThemeProvider>
-	  );
+export default function MainLayout() {
+  return (
+    <View style={styles.container}>
+      <TopBar />
+      <View style={styles.content}>
+        <Slot />
+      </View>
+      <NavigationBar />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1, // Para que el contenido ocupe el espacio restante
+  },
+});
