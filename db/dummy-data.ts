aadadeec5d907878e384893e272071e58a648922
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-04-03 17:35:24 -0300

    chore(web): remove /api/aith

--- db/dummy-data.ts
+++ db/dummy-data.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 
 import postgres from "postgres";
 import { fieldsTable, usersTable } from "./schema";
-import { sql } from "drizzle-orm";
+import { InferInsertModel, sql } from "drizzle-orm";
 
 await (async function main() {
     const client = postgres(process.env.DATABASE_URL!, { prepare: false });
@@ -10,35 +10,54 @@ await (async function main() {
 
     const user_id = 'da593797-983f-4647-a4cf-c05e2c796b30'; // tpietravallo@itba.edu.ar
 
-    const dummyData = [
+    const dummyData: (typeof fieldsTable.$inferInsert)[] = [
         {
             name: "V11", 
-            location: sql`ST_SetSRID(ST_MakePoint(-34.6117189,-58.4100384), 4326)`,
+            location: { x: -34.6117189, y: -58.4100384 },
             owner: user_id,
             avatar: "https://www.github.com/itba-tpietravallo.png", // ITBA Logo
             images: [
                 "https://xqliokcqwbwivehrgaft.supabase.co/storage/v1/object/public/venues/da593797-983f-4647-a4cf-c05e2c796b30/connor-coyne-OgqWLzWRSaI-unsplash.jpg",
                 "https://xqliokcqwbwivehrgaft.supabase.co/storage/v1/object/public/venues/da593797-983f-4647-a4cf-c05e2c796b30/fancy-crave-qowyMze7jqg-unsplash.jpg",
-            ]
+            ],
+            'city': "Buenos Aires",
+            'street_number': "1234",
+            'street': "Av. Corrientes",
+            'neighborhood': "Centro",
+            'sports': ["soccer", "basketball"],
+            description: "Un lugar para jugar al futbol y al basquet",
         },
         {
             name: "Club de Amigos",
-            location: sql`ST_SetSRID(ST_MakePoint(-34.5729272,-58.4120894), 4326)`,
+            location: { x: -34.5729272, y: -58.4120894 },
             owner: user_id,
             avatar: "https://www.github.com/itba-tpietravallo.png", // ITBA Logo
             images: [
                 "https://xqliokcqwbwivehrgaft.supabase.co/storage/v1/object/public/venues/da593797-983f-4647-a4cf-c05e2c796b30/izuddin-helmi-adnan-K5ChxJaheKI-unsplash.jpg",
                 "https://xqliokcqwbwivehrgaft.supabase.co/storage/v1/object/public/venues/da593797-983f-4647-a4cf-c05e2c796b30/jason-charters-IorqsMssQH0-unsplash.jpg",
-            ]
+            ],
+            'city': "Buenos Aires",
+            'street_number': "5678",
+            'street': "Av. Libertador",
+            'neighborhood': "Belgrano",
+            'sports': ["soccer", "basketball"],
+            description: "Un lugar para jugar al futbol y al basquet",
         },
         {
             name: "Estadio Monumental",
-            location: sql`ST_SetSRID(ST_MakePoint(-34.5455258,-58.4522481), 4326)`,
+            location: { x: -34.5455258, y: -58.4522481 },
             owner: user_id,
             avatar: "https://www.github.com/itba-tpietravallo.png", // ITBA Logo
             images: [
                 "https://xqliokcqwbwivehrgaft.supabase.co/storage/v1/object/public/venues/da593797-983f-4647-a4cf-c05e2c796b30/river.jpg"
-            ]
+            ],
+            'city': "Buenos Aires",
+            'street_number': "910",
+            'street': "Av. Figueroa Alcorta",
+            'neighborhood': "Nuñez",
+            'sports': ["soccer"],
+            description: "El estadio de River Plate",
+            
         }
     ];
 
