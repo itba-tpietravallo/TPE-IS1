c248f2858b9d8fbf4a17a7b8cc688c3424ea965e
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-04-03 15:52:30 -0300

    feat: /api/auth redirect callback to supabase

--- web/app/routes/api/auth/_auth.tsx
+++ web/app/routes/api/auth/_auth.tsx
@@ -6,16 +6,8 @@ const REDIRECT_STATUS = 307;
 export async function loader(args: LoaderFunctionArgs) {
 	const params = new URL(args.request.url).searchParams;
 	const headers = new Headers(args.request.headers);
-	headers.set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-	headers.set("Pragma", "no-cache");
-	headers.set("Expires", "0");
-	headers.set(
-		"Location",
-		`${new URL(SUPABASE_URL_CALLBACK).origin}${new URL(SUPABASE_URL_CALLBACK).pathname}?${params}`,
-	);
 
-	return new Response(null, {
-		status: REDIRECT_STATUS,
-		headers,
-	});
+	await fetch(`${new URL(SUPABASE_URL_CALLBACK).origin}${new URL(SUPABASE_URL_CALLBACK).pathname}?${params}`, { headers });
+
+	return new Response(null, { status: 200 });
 }
