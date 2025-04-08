8e50242e8298f015100aef4327dd656898010c39
felipeboya <fboyaciyan@itba.edu.ar>
2025-04-08 12:08:11 -0300

    refactor: button, landing, logo

--- web/app/routes/canchas+/_index.tsx
+++ web/app/routes/canchas+/_index.tsx
@@ -1,60 +1,62 @@
 import { MapPin } from "lucide-react";
 import { Card, CardTitle, CardDescription, CardContent } from "~/components/ui/card";
+import { Link } from "@remix-run/react";
 
 type FieldPreviewProps = {
-    name: string;
-    img: string; /* aca ni idea como poner*/
-    location: string;
-}
-
-type FieldsPreviewGridProps = {
-    fields: FieldPreviewProps[]; // Array of fields
+	name: string;
+	img: string /* aca ni idea como poner*/;
+	location: string;
 };
 
-export function FieldsPreviewGrid({fields}: FieldsPreviewGridProps) {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 w-full p-4">
-            {fields.map((field, index) =>
-                <FieldPreview key={index} {...field}/>
-            )}
-        </div>
-    );  
+type FieldsPreviewGridProps = {
+	fields: FieldPreviewProps[]; // Array of fields
+};
+
+export function FieldsPreviewGrid({ fields }: FieldsPreviewGridProps) {
+	return (
+		<div className="grid w-full grid-cols-1 gap-2 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+			{fields.map((field, index) => (
+				<FieldPreview key={index} {...field} />
+			))}
+		</div>
+	);
 }
 
-export function FieldPreview(props:FieldPreviewProps) {
-    const {name, location, img} = props;
-
-    return (
-    <div className="flex p-2 w-full">
-        <Card className="w-full">
-            <CardTitle className="p-4"> {name} </CardTitle>
-            <CardDescription className="flex items-center gap-2 m-3">
-                <MapPin className="w-4 h-4 text-gray-500" />
-                {location}
-                </CardDescription>
-            <CardContent className="flex justify-center" >
-                <img src={img} className="w-full h-32 object-cover rounded-lg"/>
-            </CardContent>
-        </Card>
-    </div>
-    );
+export function FieldPreview(props: FieldPreviewProps) {
+	const { name, location, img } = props;
+	return (
+		<Link to="./detalle-canchas">
+			<div className="flex w-full p-2">
+				<Card className="w-full">
+					<CardTitle className="p-4"> {name} </CardTitle>
+					<CardDescription className="m-3 flex items-center gap-2">
+						<MapPin className="h-4 w-4 text-gray-500" />
+						{location}
+					</CardDescription>
+					<CardContent className="flex justify-center">
+						<img src={img} className="h-32 w-full rounded-lg object-cover" />
+					</CardContent>
+				</Card>
+			</div>
+		</Link>
+	);
 }
 
-export default function(){
-    // esta data es para ver como se veria. Falta que me llegue por parámetro desde la BD la info.
+export default function () {
+	// esta data es para ver como se veria. Falta que me llegue por parámetro desde la BD la info.
 
-    const fields = [
-        { name: "Voley 1", img: "medidas_sm.jpg", location: "Virrey del Pino 1818" },
-        { name: "Fulbo 2", img: "medidas_sm.jpg", location: "San Martin 1234" },
-        { name: "Hockey 3", img: "medidas_sm.jpg", location: "Corrientes 5678" },
-        { name: "Mini golf 4", img: "medidas_sm.jpg", location: "Lavalle 91011" },
-        { name: "Ajedrez 5", img: "medidas_sm.jpg", location: "Belgrano 1213" },
-    ];
-    
-    return (
-        <div>
-            <br/>
-            <FieldsPreviewGrid fields={fields}/>
-        </div>
-    );
+	const fields = [
+		{ name: "Voley 1", img: "medidas_sm.jpg", location: "Virrey del Pino 1818" },
+		{ name: "Fulbo 2", img: "medidas_sm.jpg", location: "San Martin 1234" },
+		{ name: "Hockey 3", img: "medidas_sm.jpg", location: "Corrientes 5678" },
+		{ name: "Mini golf 4", img: "medidas_sm.jpg", location: "Lavalle 91011" },
+		{ name: "Ajedrez 5", img: "medidas_sm.jpg", location: "Belgrano 1213" },
+	];
+
+	return (
+		<div>
+			<br />
+			<FieldsPreviewGrid fields={fields} />
+		</div>
+	);
 }
--- web/app/routes/canchas+/_layout.tsx
+++ web/app/routes/canchas+/_layout.tsx
@@ -1,60 +1,57 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { Link, Outlet, useLoaderData } from "@remix-run/react";
-import { Button } from "~/components/ui/button";
+import { Button, MyButton } from "~/components/ui/button";
 import { authenticateUser } from "~/lib/auth.server";
 
 const MenuBarLinks = [
-    { name: 'Tus canchas', href: '/canchas' },
-    { name: 'Agregar nueva cancha', href: '/canchas/nueva' },
-]
+	{ name: "Tus canchas", href: "/canchas" },
+	{ name: "Agregar nueva cancha", href: "/canchas/nueva" },
+];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-    return await authenticateUser(request);
-}
+	return await authenticateUser(request);
+};
 
-export default function CanchasLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    const { avatar_url } = useLoaderData<typeof loader>();
-    return (
-        <div>
-            <nav className="absolute top-0 left-0 right-0 bg-white shadow-md px-6 py-3 flex justify-between items-center z-10">
-                <ul className="flex gap-4 flex-row items-center">
-                    <div className="font-bold p-1 px-2 mx-2 outline rounded"><img src="../matchpoint-logo.png" className="w-[100px] h-[45px]"/></div> {/* @todo Reemplazar por logo */}
-                    {
-                        MenuBarLinks.map((link) => {
-                            return <li key={link.name}>
-                                <Link to={link.href}><Button>{link.name}</Button></Link>
-                            </li>
-                        })
-                    }
-                </ul>
-                <div>
-                    <Button variant={"outline"} className="h-fit">Perfil <ProfilePictureAvatar str={avatar_url}/></Button>
-                </div>
-            </nav>
-            <main className="pt-16">
-                <Outlet/>
-            </main>
-        </div>
-    )
+export default function CanchasLayout({ children }: { children: React.ReactNode }) {
+	const { avatar_url } = useLoaderData<typeof loader>();
+	return (
+		<div>
+			<nav className="absolute left-0 right-0 top-0 z-10 flex items-center justify-between bg-white px-6 py-3 shadow-md">
+				<ul className="flex flex-row items-center gap-4">
+					<MyButton path="..">
+						<img src="../matchpoint-logo.png" className="h-[45px] w-[100px]" />
+					</MyButton>{" "}
+					{MenuBarLinks.map((link) => {
+						return (
+							<li key={link.name}>
+								<Link to={link.href}>
+									<Button>{link.name}</Button>
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+				<div>
+					<Button variant={"outline"} className="h-fit">
+						Perfil <ProfilePictureAvatar str={avatar_url} />
+					</Button>
+				</div>
+			</nav>
+			<main className="pt-16">
+				<Outlet />
+			</main>
+		</div>
+	);
 }
 
 export function ProfilePictureAvatar({ str }: { str?: string | null } = { str: null }) {
-    return <div className="flex items-center justify-center aspect-square w-10 rounded-full overflow-hidden">
-    {
-        str ?
-        <img 
-            src={str}
-            alt="Profile"
-            className="w-full h-full object-cover"
-        />
-        :
-        <div className="w-full h-full bg-gray-400 flex items-center justify-center text-white">
-            {'U'}
-        </div>
-    }
-    </div>
+	return (
+		<div className="flex aspect-square w-10 items-center justify-center overflow-hidden rounded-full">
+			{str ? (
+				<img src={str} alt="Profile" className="h-full w-full object-cover" />
+			) : (
+				<div className="flex h-full w-full items-center justify-center bg-gray-400 text-white">{"U"}</div>
+			)}
+		</div>
+	);
 }
