bbefdc8153eedfc0e5ca5ea1c300097695c9f57d
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-04-09 13:23:30 -0300

    chore: merge mobile (squash)
    commit 2a2e64a8f7edf1ac4bf1f38eeef54cda039accde
    Author: Tomás Pietravallo <hello@tomaspietravallo.com>
    Date:   Wed Apr 9 13:20:04 2025 -0300
    
        chore(mobile): add missing dependency
    
    commit 5ed01d01f0f00c0395aed5f8cf7db6e3565ae6e8
    Merge: 61bceeb 0a9c9a8
    Author: Lola Diaz-Varela <ldiazvarela@itba.edu.ar>
    Date:   Wed Apr 9 10:18:49 2025 -0300
    
        Merge branch 'mobile' of https://github.com/itba-tpietravallo/TPE-IS1 into mobile
    
    commit 61bceeb3e7fd0cac550e4997d1dc52d79e697fdf
    Author: Lola Diaz-Varela <ldiazvarela@itba.edu.ar>
    Date:   Wed Apr 9 10:18:21 2025 -0300
    
        Feat: Post Team
    
    commit 0a9c9a8949a45cdd552393f04011080a5db4fa5d
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Tue Apr 8 10:35:10 2025 -0300
    
        feat[mobile]: fetch user info
    
    commit f754737da325454efbd1554be3800b7b0d60a87c
    Merge: f7a52d2 aad1e2d
    Author: Lola Diaz-Varela <ldiazvarela@itba.edu.ar>
    Date:   Tue Apr 8 09:49:57 2025 -0300
    
        Merge branch 'mobile' of https://github.com/itba-tpietravallo/TPE-IS1 into mobile
    
    commit aad1e2dc48cef4be0013ba2cd7badb140bda3394
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Mon Apr 7 18:55:51 2025 -0300
    
        error
    
    commit 88f56bb5e70c47c5365af7ae397ffcf6e24fb87f
    Merge: c8656bc 274edf8
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Sat Apr 5 12:08:15 2025 -0300
    
        Merge branch 'main' into mobile
    
    commit c8656bc9811dc9d7f22ea6f1ded991226439ac29
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Sat Apr 5 12:06:48 2025 -0300
    
        .
    
    commit f7a52d22212834c524014b73d7adf8bdc62e74db
    Author: Lola Diaz-Varela <ldiazvarela@itba.edu.ar>
    Date:   Thu Apr 3 15:38:01 2025 -0300
    
        fix: change white for smoke
    
    commit 5bd6a6b3988ccd1cd65a19dbb5ae84734d460ced
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Thu Apr 3 14:28:48 2025 -0300
    
        quick fix
    
    commit d7843c490d4773514cbfbeb938f1e6311ca7ab59
    Merge: 1ee6bc4 3d41e4e
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Thu Apr 3 14:17:52 2025 -0300
    
        Merge branch 'mobile' of https://github.com/itba-tpietravallo/TPE-IS1 into mobile
    
    commit 1ee6bc4f56773108c4f302e83b122a8526b54eac
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Thu Apr 3 14:17:29 2025 -0300
    
        pop up reserva
    
    commit 3d41e4ea5352a02f214231b83ac2a577a2d4e7dd
    Merge: 526f77e 485afea
    Author: Tomás Pietravallo <hello@tomaspietravallo.com>
    Date:   Thu Apr 3 12:49:18 2025 -0300
    
        Merge branch 'main' into mobile
    
    commit 526f77e15f82de9846fa0a9c31e9b47ec5bbb231
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Mon Mar 31 19:19:13 2025 -0300
    
        in process
    
    commit daa8a2e1f4bc27cf5a3cfad70840b158466f921d
    Author: Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
    Date:   Sun Mar 30 23:51:05 2025 -0300
    
        first glance - raw
    
        TO-DO:
        que lo primero que vea sea el auth
        cosas del feed
    
    commit ec3c0b0b2e437051bd9cb9d8a03e630063be2245
    Author: Tomás Pietravallo <hello@tomaspietravallo.com>
    Date:   Sun Mar 30 16:54:43 2025 -0300
    
        feat(mobile): oauth deeplink and protected screens
    
    commit 91e8f73a95d2b5a5da69b2059979526a0ff2f37a
    Author: Tomás Pietravallo <hello@tomaspietravallo.com>
    Date:   Sun Mar 30 11:23:32 2025 -0300
    
        feat(mobile): oauth sign in w/ google
    
    commit bbfdeda3900aa5574ecebdf8f8e8a0f9e42d26b0
    Author: Tomás Pietravallo <hello@tomaspietravallo.com>
    Date:   Sat Mar 29 21:42:34 2025 -0300
    
        feat: basic auth screen
    
    commit 8eaf71c5eea9130281587205784dd885d586fc5a
    Author: Tomás Pietravallo <hello@tomaspietravallo.com>
    Date:   Sat Mar 29 21:42:23 2025 -0300
    
        chore: reset-project

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,13 +1,7 @@
-import {
-    Links,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import styles from "./tailwind.css?url"
+import styles from "./tailwind.css?url";
 
 export const links: LinksFunction = () => [
 	{ rel: "stylesheet", href: styles },
