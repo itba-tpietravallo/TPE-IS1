4e1a858fe5297a8990ad9acf5f1a634941e9a719
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-04-19 01:00:00 -0300

    chore: fix FieldPreview grid row height

--- web/app/routes/canchas+/_index.tsx
+++ web/app/routes/canchas+/_index.tsx
@@ -5,96 +5,89 @@ import { useLoaderData } from "@remix-run/react";
 import { createBrowserClient } from "@supabase/ssr";
 import { useEffect, useState } from "react";
 import { Link } from "@remix-run/react";
+import clsx from "clsx";
 
 type FieldPreviewProps = {
-    id: string;
-    name: string;
-    img: string; /* aca ni idea como poner*/
-    location: string;
-}
+	id: string;
+	name: string;
+	img: string /* aca ni idea como poner*/;
+	location: string;
+};
 
 export function loader(args: LoaderFunctionArgs) {
-    const env = {
-        SUPABASE_URL: process.env.SUPABASE_URL!,
-        SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!,
-    }
-    
-    return ({
-        env,
-        URL_ORIGIN: new URL(args.request.url).origin
-    });
+	const env = {
+		SUPABASE_URL: process.env.SUPABASE_URL!,
+		SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!,
+	};
+
+	return {
+		env,
+		URL_ORIGIN: new URL(args.request.url).origin,
+	};
 }
 
 type FieldsPreviewGridProps = {
-    fields: FieldPreviewProps[];
+	fields: FieldPreviewProps[];
 };
 
-export function FieldsPreviewGrid({fields}: FieldsPreviewGridProps) {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 w-full p-4">
-            {fields.map((field, index) => (
-                
-                <FieldPreview key={field.id} {...field} />
-
-            ))}
-        </div>
-    );  
+export function FieldsPreviewGrid({ fields }: FieldsPreviewGridProps) {
+	return (
+		<div className="grid w-full grid-cols-1 gap-2 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+			{fields.map((field, index) => (
+				<FieldPreview key={field.id} {...field} />
+			))}
+		</div>
+	);
 }
 
-export function FieldPreview(props:FieldPreviewProps) {
-    const {id, name, location, img} = props;
-
-    return (
-    <div className="flex p-2 w-full">
-       <Link to={`/canchas/detalle-canchas/${id}`} className="w-full">
-            <Card className="w-full bg-[#f2f4f3] border border-[#f2f4f3] transition-all duration-200 hover:bg-[#f18f01] hover:border-[#f18f01]">
-                <CardTitle className="p-4"> {name} </CardTitle>
-                <CardDescription className="flex items-center gap-2 m-3">
-                    <MapPin className="w-4 h-4 text-gray-500" />
-                    {location}
-                    </CardDescription>
-                <CardContent className="flex justify-center" >
-                    <img src={img} className="w-full h-32 object-cover rounded-lg"/>
-                </CardContent>
-            </Card>
-        </Link>
-    </div>
-    );
+export function FieldPreview(props: FieldPreviewProps & { className?: string }) {
+	const { id, name, location, img } = props;
+	return (
+		<div className={clsx("flex h-full min-h-full w-full grow p-2", props?.className)}>
+			<Link to={`/canchas/detalle-canchas/${id}`} className="min-h-full w-full">
+				<Card className="min-h-full w-full border border-[#f2f4f3] bg-[#f2f4f3] transition-all duration-200 hover:border-[#f18f01] hover:bg-[#f18f01]">
+					<CardTitle className="p-4">{name}</CardTitle>
+					<CardDescription className="m-3 flex items-center gap-2">
+						<MapPin className="h-4 w-4 text-gray-500" />
+						{location}
+					</CardDescription>
+					<CardContent className="flex justify-center">
+						<img src={img} className="h-32 w-full rounded-lg object-cover" />
+					</CardContent>
+				</Card>
+			</Link>
+		</div>
+	);
 }
 
+export default function () {
+	const { URL_ORIGIN, env } = useLoaderData<typeof loader>();
+	const supabase = createBrowserClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
+	const [users, setUsers] = useState<any[]>([]);
 
+	useEffect(() => {
+		supabase
+			.from("fields")
+			.select("*")
+			.then(({ data, error }) => {
+				if (error) {
+					console.error("Supabase error:", error);
+				} else {
+					setUsers(data as any[]);
+				}
+			});
+	}, []);
 
-export default function(){
+	const fields = users.map((user, i) => ({
+		id: user.id,
+		name: user.name,
+		img: user.images[0],
+		location: `${user.street} ${user.street_number}`,
+	}));
 
-    const { URL_ORIGIN, env } = useLoaderData<typeof loader>();
-    const supabase = createBrowserClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
-    const [users, setUsers] = useState<any[]>([]);
-    
-        useEffect(() => {
-            supabase
-                .from("fields")
-                .select("*")
-                .then(({ data, error }) => {
-                    if (error) {
-                        console.error("Supabase error:", error);
-                    } else {
-                        setUsers(data as any[]);
-                    }
-                });
-        }, []);
-
-        const fields = users.map((user, i) => ({
-
-            id: user.id,
-            name: user.name,
-            img: user.images[0],
-            location: `${user.street} ${user.street_number}`,
-          }));
-    
-    return (
-        <div className="flex min-h-screen bg-[#223332]">
-            <br/>
-            <FieldsPreviewGrid fields={fields}/>
-        </div>
-    );
+	return (
+		<div className="h-full bg-[#223332]">
+			<FieldsPreviewGrid fields={fields} />
+		</div>
+	);
 }
