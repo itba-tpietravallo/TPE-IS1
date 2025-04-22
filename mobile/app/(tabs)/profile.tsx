f47168dcc3ae242f08dc9e7ac0aca245de5e39f7
Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
2025-04-22 11:01:14 -0300

    feat(mobile): mis reservas done

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -1,8 +1,9 @@
 import { supabase } from "@/lib/supabase";
 import { Button, Image } from "@rneui/themed";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Modal } from "react-native";
 import { useEffect, useState } from "react";
 import { Session } from "@supabase/supabase-js";
+import ReservationInfo from "@/components/reservationInfo";
 
 type User = {
 	id: string;
@@ -16,7 +17,11 @@ type Reservation = {
 	date: string;
 	field: {
 		name: string;
-		location: string;
+		//location: string;
+		street_number: string;
+		street: string;
+		neighborhood: string;
+		city: string;
 	};
 };
 
@@ -36,103 +41,111 @@ export default function Index() {
 						console.error("Error fetching user:", error);
 					} else {
 						setUser(data);
-						if (user?.user.id) {
-							supabase
-								.from("reservations")
-								.select("*")
-								.eq("owner_id", user?.user.id)
-								.order("date", { ascending: false })
-								.then(({ data, error }) => {
-									if (error) {
-										console.error("Error fetching reservations:", error);
-									} else {
-										setReservations(data || []);
-									}
-								});
-						}
+						console.log("id:", data.id);
+						supabase
+							.from("reservations")
+							.select(
+								`*, field: fields (name, street_number, street, neighborhood, city)`, // , location: fields(location)`",
+							)
+							.eq("owner_id", data.id)
+							.order("date", { ascending: true })
+							.then(({ data, error }) => {
+								if (error) {
+									console.error("Error fetching reservations:", error);
+								} else {
+									setReservations(data || []);
+								}
+							});
 					}
 				});
 		});
 	}, []);
 
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const [selectedReservation, setSelectedReservation] = useState<Reservation | null>(null);
+
+	const handleOpenModal = (reservation: Reservation) => {
+		setSelectedReservation(reservation);
+		setIsModalVisible(true);
+	};
+	const handleCloseModal = () => {
+		setIsModalVisible(false);
+		setSelectedReservation(null);
+	};
+
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<View
-				style={{
-					marginBottom: 20,
-					justifyContent: "space-between",
-					alignContent: "center",
-				}}
-			>
-				<View style={{ padding: 20, alignContent: "center" }}>
-					<Text
-						style={{
-							fontSize: 30,
-							fontWeight: "bold",
-							color: "gray",
-							paddingBottom: 20,
-						}}
-					>
-						Usuario:
-					</Text>
-					<View style={{ alignItems: "center" }}>
+		<View style={styles.containter}>
+			<View>
+				<View>
+					<View style={{ alignItems: "center", padding: 30 }}>
 						<Image source={{ uri: user?.avatar_url }} style={{ width: 100, height: 100 }} />
 						<Text style={{ fontSize: 30, fontWeight: "bold", paddingTop: 20 }}>{user?.full_name}</Text>
+						<Button
+							buttonStyle={styles.buttonContainer}
+							title="Cerrar sesión"
+							onPress={async () => {
+								const { error } = await supabase.auth.signOut({ scope: "local" });
+								if (error) {
+									console.error("Error signing out:", error.message);
+								}
+								console.log("Signed out successfully");
+							}}
+						/>
 					</View>
 				</View>
 			</View>
 
-			<View style={{ padding: 20, alignContent: "center" }}>
+			<View>
 				<Text
 					style={{
 						fontSize: 30,
 						fontWeight: "bold",
 						color: "gray",
-						paddingBottom: 20,
 						textAlign: "left",
+						paddingBottom: 10,
 					}}
 				>
 					Mis reservas:
 				</Text>
-				{reservations.length > 0 ? (
-					reservations.map((reservation) => (
-						<View
-							key={reservation.id}
-							style={{
-								marginBottom: 10,
-								borderWidth: 1,
-								borderColor: "#ccc",
-								padding: 10,
-								borderRadius: 5,
-							}}
-						>
-							<Text style={{ fontWeight: "bold" }}>Cancha: {reservation.field?.name}</Text>
-							<Text>Ubicación: {reservation.field?.location}</Text>
-							<Text>Fecha: {reservation.date}</Text>
-							<Text>Hora: {reservation.start_time}</Text>
-						</View>
-					))
-				) : (
-					<Text>No tienes reservas.</Text>
-				)}
+				<View style={{ padding: 20, borderWidth: 1, borderColor: "#ccc", borderRadius: 5 }}>
+					{reservations.length > 0 ? (
+						reservations.map((reservation, i) => (
+							<View
+								key={reservation.id}
+								style={{
+									flexDirection: "row",
+									justifyContent: "space-between",
+									padding: 10,
+									borderBottomWidth: 1,
+									borderBottomColor: "#ccc",
+								}}
+							>
+								<Text>{reservation.date}</Text>
+								<Text style={{ fontWeight: "bold" }}>{reservation.field.name}</Text>
+								<TouchableOpacity onPress={() => handleOpenModal(reservation)}>
+									<Image
+										style={{ width: 20, height: 20 }}
+										source={require("@/assets/images/info.png")}
+									/>
+								</TouchableOpacity>
+							</View>
+						))
+					) : (
+						<Text>No tienes reservas.</Text>
+					)}
+				</View>
 			</View>
-			<Button
-				buttonStyle={styles.buttonContainer}
-				title="Cerrar sesión"
-				onPress={async () => {
-					const { error } = await supabase.auth.signOut({ scope: "local" });
-					if (error) {
-						console.error("Error signing out:", error.message);
-					}
-					console.log("Signed out successfully");
-				}}
-			/>
+			<Modal style={styles.modal} visible={isModalVisible} transparent={true} onRequestClose={handleCloseModal}>
+				<View style={styles.centeredView}>
+					<ReservationInfo
+						onClose={handleCloseModal}
+						field_name={selectedReservation?.field?.name || ""}
+						date={[selectedReservation?.date || ""]}
+						time={selectedReservation?.start_time || ""}
+						location={`${selectedReservation?.field?.street || ""} ${selectedReservation?.field?.street_number || ""}, ${selectedReservation?.field?.neighborhood || ""}, ${selectedReservation?.field?.city || ""}`}
+					/>
+				</View>
+			</Modal>
 		</View>
 	);
 }
@@ -142,6 +155,26 @@ const styles = StyleSheet.create({
 		marginTop: 10,
 		padding: 10,
 		borderRadius: 15,
-		backgroundColor: "#CC0000",
+		//		backgroundColor: "#CC0000",
+		backgroundColor: "#223332",
+		justifyContent: "center",
+	},
+	containter: {
+		justifyContent: "space-between",
+		padding: 20,
+	},
+	centeredView: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+	},
+	modal: {
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 20,
+		overflow: "hidden",
+		width: "90%",
+		maxWidth: 400,
 	},
 });
---
+++ mobile/components/reservationInfo.tsx
@@ -0,0 +1,87 @@
+interface infoProps {
+	onClose: () => void;
+
+	field_name: string;
+	date: string[];
+	time: string;
+	location: string;
+}
+import { useEffect, useRef, useState } from "react";
+import { Alert, Text, View, TouchableOpacity, Image } from "react-native";
+import { Button } from "@rneui/themed";
+
+function ReservationInfo({ onClose, field_name, date, time, location }: infoProps) {
+	return (
+		<View
+			style={{
+				backgroundColor: "white",
+				borderRadius: 20,
+				justifyContent: "center",
+				margin: 20,
+				//color: "#00ff00",
+				overflow: "hidden",
+				padding: 10,
+			}}
+		>
+			<TouchableOpacity style={{ padding: 10, alignItems: "flex-end" }} onPress={onClose}>
+				<Image style={{ width: 20, height: 20, marginTop: 10 }} source={require("@/assets/images/close.png")} />
+			</TouchableOpacity>
+
+			<View style={{ padding: 20 }}>
+				<View
+					style={{
+						flexDirection: "row",
+						paddingBottom: 10,
+						alignContent: "center",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<Text style={{ fontWeight: "bold", fontSize: 24 }}>{field_name}</Text>
+				</View>
+				<View style={{ flexDirection: "row", paddingBottom: 10, alignContent: "center", alignItems: "center" }}>
+					<Text style={{ color: "gray" }}>Fecha: </Text>
+					<Text style={{ fontSize: 16 }}>{date}</Text>
+				</View>
+				<View style={{ flexDirection: "row", paddingBottom: 10, alignContent: "center", alignItems: "center" }}>
+					<Text style={{ color: "gray" }}>Horario: </Text>
+					<Text style={{ fontSize: 16 }}>{time}</Text>
+				</View>
+				<View
+					style={{
+						flexDirection: "row",
+						paddingBottom: 10,
+						alignContent: "center",
+						alignItems: "center",
+						flexWrap: "wrap",
+					}}
+				>
+					<Text style={{ color: "gray" }}>Dirección: </Text>
+					<Text
+						style={{
+							fontSize: 16,
+							flexWrap: "wrap", // Permite que el texto salte a la siguiente línea
+						}}
+					>
+						{location}
+					</Text>
+				</View>
+			</View>
+
+			<Button
+				buttonStyle={{
+					marginTop: 10,
+					padding: 10,
+					borderRadius: 15,
+					//		backgroundColor: "#CC0000",
+					backgroundColor: "#223332",
+					justifyContent: "center",
+				}}
+				title="Cancelar reserva"
+				onPress={onClose}
+			/>
+		</View>
+	);
+}
+
+export default ReservationInfo;
