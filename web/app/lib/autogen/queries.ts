5c22cdd7ff431a6fd5ef586942f15aaa291b9558
tomaspietravallo <62444821+tomaspietravallo@users.noreply.github.com>
2025-04-28 12:16:08 +0000

    bot(db): generate database types

--- web/app/lib/autogen/queries.ts
+++ web/app/lib/autogen/queries.ts
@@ -1,3 +1,7 @@
+// ===============================================================
+// Source file: db/queries.ts
+// ===============================================================
+
 import { SupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "./database.types";
 
@@ -5,6 +9,42 @@ export function getAllFields(supabase: SupabaseClient<Database>) {
 	return supabase.from("fields").select("*");
 }
 
+export function getAllFieldsByOwner(supabase: SupabaseClient<Database>, ownerId: string) {
+	return supabase.from("fields").select("*").eq("owner", ownerId);
+}
+
+export function getFieldById(supabase: SupabaseClient<Database>, fieldId: string) {
+	return supabase.from("fields").select("*").eq("id", fieldId).single();
+}
+
 export function getAllSports(supabase: SupabaseClient<Database>) {
-	return supabase.from("sports").select("*");
+	return supabase.from("sports").select("name");
+}
+
+export function insertNewField(
+	supabase: SupabaseClient<Database>,
+	field: Database["public"]["Tables"]["fields"]["Insert"],
+) {
+	return supabase.from("fields").insert(field);
+}
+
+export function getAllReservationsForFieldById(supabase: SupabaseClient<Database>, fieldId: string) {
+	return supabase.from("reservations").select("*").eq("field_id", fieldId);
+}
+
+export function getAllReservationTimeSlots(supabase: SupabaseClient<Database>, fieldId: string) {
+	return supabase.from("reservations").select("date_time").eq("field_id", fieldId);
+	// .gte("start_time", startTime);
+}
+
+export function getAllTeams(supabase: SupabaseClient<Database>) {
+	return supabase.from("teams").select("team_id, name, sport, description, images, players");
+}
+
+export function getUserSession(supabase: SupabaseClient<Database>) {
+	return supabase.from("users").select("id, full_name, avatar_url").eq("id", `auth.uid()`).single();
+}
+
+export function getAllUsers(supabase: SupabaseClient<Database>) {
+	return supabase.from("users").select("id, full_name, avatar_url");
 }
