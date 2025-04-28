40ac08284854705c21921a59443c150a1af521e9
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-04-28 11:09:29 -0300

    build: create intermediate directories

--- db/autogen.sh
+++ db/autogen.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ ! -d "../mobile/lib/autogen" ]]; then
+  mkdir -p ../mobile/lib/autogen
+fi
+
+if [[ ! -d "../web/app/lib/autogen" ]]; then
+  mkdir -p ../web/app/lib/autogen
+fi
+
 cp ./database.types.ts ../mobile/lib/autogen/database.types.ts
 cp ./database.types.ts ../web/app/lib/autogen/database.types.ts
 
