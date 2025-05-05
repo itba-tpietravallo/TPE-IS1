85179beed599a4d62664546f98fc8a45817bf025
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-05-05 10:21:58 -0300

    fix: db field name, broke api requests

--- db/schema.ts
+++ db/schema.ts
@@ -117,7 +117,7 @@ export const reservationsTable = pgTable(
 		owner_id: uuid()
 			.notNull()
 			.references(() => usersTable.id, { onDelete: "cascade" }),
-		payment_id: bigint({ mode: "number" }).default(sql`NULL`),
+		payments_id: bigint({ mode: "number" }).default(sql`NULL`),
 	},
 	(table) => [
 		pgPolicy("reservations - select authenticated", {
