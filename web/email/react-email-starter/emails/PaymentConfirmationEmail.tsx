b5bcf2f361f76b7702733017e4f368189b829bc2
Lucia Oliveto <luciamoliveto@gmail.com>
2025-05-26 12:38:54 -0300

    refactor: payment confirmation email

--- web/email/react-email-starter/emails/PaymentConfirmationEmail.tsx
+++ web/email/react-email-starter/emails/PaymentConfirmationEmail.tsx
@@ -6,6 +6,7 @@ interface PaymentConfirmationEmailProps {
 	payment_id: string;
 	field_name: string;
 	reservation_date: Date;
+	team_name?: string;
 }
 
 export function PaymentConfirmationEmail({
@@ -14,6 +15,7 @@ export function PaymentConfirmationEmail({
 	payment_id,
 	field_name,
 	reservation_date,
+	team_name,
 }: PaymentConfirmationEmailProps) {
 	const date = reservation_date.toLocaleDateString("es-ES", {
 		day: "2-digit",
@@ -34,7 +36,7 @@ export function PaymentConfirmationEmail({
 				<Body className="bg-white px-2 font-sans">
 					<Section className="">
 						<Img
-							src="https://xqliokcqwbwivehrgaft.supabase.co/storage/v1/object/public/email-photos//matchpoint-logo.png"
+							src="https://matchpointapp.com.ar/matchpoint-logo.png"
 							alt="MatchPoint Logo"
 							className="h-[45px] w-auto"
 						/>
@@ -42,14 +44,15 @@ export function PaymentConfirmationEmail({
 					<Container className="mx-auto my-[20px] max-w-[465px] rounded border border-solid border-[#eaeaea]">
 						<Container className="rounded-t bg-[#f18f01]">
 							<Heading className="mx-0 my-[30px] p-0 text-center text-[28px] font-normal text-[#223332]">
-								Reserva confirmada
+								Pago confirmado
 							</Heading>
 						</Container>
 						<Section className="bg-[#f2f4f3] p-4 text-[#223332]">
 							<Text className="text-[20px]">Hola {player_name}!</Text>
 							<Text className="text-[15px]">
-								Tu reserva de la cancha <strong>{field_name}</strong> para el día{" "}
-								<strong>{date}</strong> fue realizada con éxito.
+								Tu pago a la cancha <strong>{field_name}</strong>{" "}
+								{team_name ? `con el equipo ${team_name} ` : ""}
+								para el día <strong>{date}</strong> fue realizada con éxito.
 							</Text>
 							<br />
 							<Section>
