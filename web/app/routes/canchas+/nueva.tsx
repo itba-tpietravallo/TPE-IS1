9f72bc87b10b21e7786e4dc8c10f257d9b0d101e
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-05-27 15:39:31 -0300

    chore: bypass missing coordinate, maps geocoding

--- web/app/routes/canchas+/nueva.tsx
+++ web/app/routes/canchas+/nueva.tsx
@@ -169,7 +169,7 @@ export function NewField() {
 					"Por favor, ingrese una dirección válida para ubicarla en el mapa antes de publicar.",
 				);
 				setFormError(true);
-				return;
+				// return;
 			}
 
 			const files = data.image || [];
@@ -208,7 +208,7 @@ export function NewField() {
 				sports: data.sports.map((s) => s.value),
 				images: uploadedImageUrls,
 				price: Number(data.price),
-				location: `POINT(${latitude} ${longitude})`,
+				location: `POINT(${latitude || 0} ${longitude || 0})`,
 				description: data.description || "",
 				avatar_url: user.avatar_url,
 			});
