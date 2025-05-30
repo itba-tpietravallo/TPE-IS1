a79d54f8c17f3b92a1adf733654be0b226d7b95f
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-05-30 17:34:15 -0300

    feat: dynamic mobile db credentials (#18)
    * feat: dynamically retrieve database credentials
    
    * feat: add sentry error monitoring
    
    * chore: @expo/config-plugins as dev dep
    
    * chore: remove logs

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,7 +2,7 @@ import { SplashScreen, Stack } from "expo-router";
 
 import "react-native-url-polyfill/auto";
 import { useState, useEffect } from "react";
-import { supabase } from "../lib/supabase";
+import { initializeSupabaseClient, supabase } from "../lib/supabase";
 import Auth from "../components/Auth";
 import { Session } from "@supabase/supabase-js";
 import { StatusBar } from "expo-status-bar";
@@ -17,18 +17,38 @@ Sentry.init({
 	sendDefaultPii: true,
 });
 
-function RootLayout() {
+export function RootLayout() {
+	const init = initializeSupabaseClient();
+	const [loaded, setLoaded] = useState(false);
 	const [session, setSession] = useState<Session | null>(null);
 
-	useEffect(() => {
-		supabase.auth.getSession().then(({ data: { session } }) => {
-			setSession(session);
-		});
+	init.then(() => {
+		setLoaded(true);
+	});
 
-		supabase.auth.onAuthStateChange((_event, session) => {
-			setSession(session);
+	// This loads instantly, but is set up so it can await fonts or other critical resources.
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+
+	useEffect(() => {
+		init.then(() => {
+			supabase.auth.getSession().then(({ data: { session } }) => {
+				setSession(session);
+			});
+
+			supabase.auth.onAuthStateChange((_event, session) => {
+				setSession(session);
+			});
 		});
 	}, []);
+
+	if (!loaded) {
+		return null;
+	}
 
 	const queryClient = new QueryClient({
 		defaultOptions: {
