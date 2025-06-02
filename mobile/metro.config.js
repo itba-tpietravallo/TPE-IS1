ea91f8f345030fc78046e4b0dc38915d7355e9e1
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-06-02 12:18:25 -0300

    feat: add sentry error monitoring (#17)

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,8 +1,8 @@
 // metro.config.js
-const { getDefaultConfig } = require("expo/metro-config");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+let config = getSentryExpoConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = false;
 
