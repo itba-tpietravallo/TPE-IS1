a96b80a1800f904dd3b1d491d77f7ed7724982a8
Lola Diaz-Varela <ldiazvarela@itba.edu.ar>
2025-06-12 15:31:45 -0300

    Updated para el uso de buckets

--- mobile/lib/uploadImage.ts
+++ mobile/lib/uploadImage.ts
@@ -1,24 +1,35 @@
-import { supabase } from "@/lib/supabase";
+// uri: string (ruta local de la imagen en el dispositivo)
+// userId: string (opcional, para personalizar el nombre del archivo)
+import * as FileSystem from "expo-file-system";
 
-export async function uploadImageToStorage(uri: string, userId: string) {
-	const response = await fetch(uri);
-	const blob = await response.blob();
-	const fileExt = uri.split(".").pop();
-	const fileName = `${userId}_${Date.now()}.${fileExt}`;
-	const filePath = `teams/${fileName}`;
+export async function uploadImageToStorage(uri: string, userId: string = "anon"): Promise<string> {
+	const fileName = uri.split("/").pop() || `image_${Date.now()}.jpg`;
 
-	let { error } = await supabase.storage.from("matchpoint-images").upload(filePath, blob, {
-		contentType: blob.type,
-		upsert: true,
+	const apiUrl = "/api/v1/upload";
+	//const apiUrl = process.env.EXPO_PUBLIC_API_URL?.replace(/\/$/, "") + "/api/v1/upload";
+	//const apiUrl = "http://192.168.0.10:8081/api/v1/upload";
+
+	const res = await fetch(apiUrl, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ fileName }),
 	});
+	console.log("Status:", res.status); //debug
+	console.log("Texto:", await res.text());
 
-	//if (error) throw error;
-	if (error) {
-		console.log("Upload error:", error);
-		throw error;
-	}
+	if (!res.ok) throw new Error("No se pudo obtener la URL de subida");
+	const { signedPUTURL, downloadURL } = await res.json();
 
-	const { data } = supabase.storage.from("matchpoint-images").getPublicUrl(filePath);
+	//lee el archivo como binario
+	const fileData = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
+	const fileBuffer = Uint8Array.from(atob(fileData), (c) => c.charCodeAt(0));
 
-	return data.publicUrl;
+	const uploadRes = await fetch(signedPUTURL, {
+		method: "PUT",
+		headers: { "Content-Type": "application/octet-stream" },
+		body: fileBuffer,
+	});
+	if (!uploadRes.ok) throw new Error("Error al subir la imagen al bucket");
+
+	return downloadURL;
 }
