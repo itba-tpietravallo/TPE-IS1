c23b9f6d870a3a64b00d7ddb0680636ffdcc6b2d
felipeboya <fboyaciyan@itba.edu.ar>
2025-07-11 18:22:14 -0300

    Update profile.tsx

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -115,8 +115,8 @@ const menuItems = [
 	{ label: "Equipos", icon: "people-group", action: () => router.push("/(tabs)/profileMenu/teams") },
 	// { label: "Mis torneos", icon: "medal", action: () => router.push("/(tabs)/profileMenu/teams") },
 	{ label: "Pendientes", icon: "spinner", action: () => router.push("/(tabs)/profileMenu/pendings") },
-	{ label: "Favoritos", icon: "user-group", action: () => router.push("/(tabs)/profileMenu/favUsers") },
 	{ label: "Chats", icon: "comments", action: () => router.push("/(tabs)/profileMenu/chats") },
+	{ label: "Usuarios Favoritos", icon: "user-group", action: () => router.push("/(tabs)/profileMenu/favUsers") },
 ];
 
 export function ProfileMenuList() {
