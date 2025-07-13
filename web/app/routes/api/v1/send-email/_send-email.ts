e6ea2cf4dd42ea76e647b05c75d6eb3d0c52518e
Tomás Pietravallo <hello@tomaspietravallo.com>
2025-07-13 14:50:19 -0300

    feat: botti mail

--- web/app/routes/api/v1/send-email/_send-email.ts
+++ web/app/routes/api/v1/send-email/_send-email.ts
@@ -3,6 +3,7 @@ import { Resend } from "resend";
 import { PaymentConfirmationEmail } from "../../../../../email/react-email-starter/emails/PaymentConfirmationEmail";
 import ReservationConfirmationEmail from "email/react-email-starter/emails/ReservationConfirmationEmail";
 import CancelledReservationEmail from "email/react-email-starter/emails/CancelledReservationEmail";
+import BottiMailFinal from "email/react-email-starter/emails/BottiMailFinal";
 
 export const action = async ({ request }: { request: Request }) => {
 	const resend = new Resend(process.env.RESEND_PAYMENT_MAILER);
@@ -48,6 +49,35 @@ export const action = async ({ request }: { request: Request }) => {
 					reservation_date: new Date(body.reservation_date),
 				}),
 			});
+		} else if (body.type === "botti_mail") {
+			const recipientsList = [
+				{ name: "Lu", email: "loliveto@itba.edu.ar" },
+				{ name: "Max", email: "mwehncke@itba.edu.ar" },
+				// { "name": "Fede", "email": "fbotti@itba.edu.ar" },
+				// { "name": "Nico", "email": "nmargenat@itba.edu.ar" },
+				// { "name": "Elian", "email": "eparedes@itba.edu.ar" },
+				// { "name": "Theo", "email": "tshlamovitz@itba.edu.ar" }
+			];
+
+			for (const recipient of recipientsList) {
+				if (!recipient.email) {
+					throw new Error(`Email not provided for ${recipient.name}`);
+				}
+
+				console.log(`Sending email to ${recipient.name} at ${recipient.email}`);
+				
+				await resend.emails.send({
+					from: "MatchPoint <no-reply@final.matchpointapp.com.ar>",
+					to: recipient.email,
+					subject: "Matchpoint te espera 🏟️",
+					cc: 'tpietravallo@itba.edu.ar',
+					react: BottiMailFinal({
+						player_name: recipient.name,
+					}),
+				});
+			}
+
+			data = { success: true, message: "Emails sent successfully to all recipients." };
 		} else {
 			throw new Error("Unsupported email type");
 		}
---
+++ web/email/react-email-starter/emails/BottiMailFinal.tsx
@@ -0,0 +1,56 @@
+import { Body, Container, Head, Heading, Html, Img, Section, Tailwind, Text } from "@react-email/components";
+
+interface BottiMailFinalEmailProps {
+	player_name: string;
+}
+
+export function BottiMailFinal({
+	player_name,
+}: BottiMailFinalEmailProps) {
+	return (
+		<Html>
+			<Head />
+			<Tailwind>
+				<Body className="bg-white px-2 font-sans">
+					<Section className="">
+						<Img
+							src="https://storage.googleapis.com/email-images-matchpointapp/matchpoint-logo.png"
+							alt="MatchPoint Logo"
+							className="h-[45px] w-auto"
+						/>
+					</Section>
+					<Container className="mx-auto my-[20px] max-w-[465px] rounded border border-solid border-[#eaeaea]">
+						<Container className="rounded-t bg-[#f18f01]">
+							<Heading className="mx-0 my-[30px] p-0 text-center text-[28px] font-normal text-white">
+								Matchpoint te espera 🏟️
+							</Heading>
+						</Container>
+						<Section className="bg-[#f2f4f3] p-4 text-[#223332]">
+							<Text className="text-[15px]">
+								Hola {player_name}!
+							</Text>
+							<Text className="text-[15px]">
+								Sabemos que tenés ganas de jugar, ¡no te quedes sin la oportunidad!
+							</Text>
+							<Text className="text-[15px]">
+								Te esperamos mañana a las 8:00 en la presentación de nuestra app, ¡ahora con mails de confirmación!
+							</Text>
+							<Text>
+								Si no podés esperar, podés empezar instalando <a href="https://storage.googleapis.com/matchpointapp-android-apks/universal.apk" className="text-[#f18f01] underline">nuestra APK de Android</a>, o <a href="https://matchpointapp.com.ar" className="text-[#f18f01] underline">visitando nuestra web</a>.
+							</Text>
+							<Text>¿Querés ver todos nuestros OKRs, C4s, roadmaps y más? <a href="https://drive.google.com/drive/folders/14sF9bespwfx1I6Y7vi2j3NuNsztg2V-S?usp=sharing" className="text-[#f18f01] underline">¡Te tenemos cubierto 😎!</a></Text>
+							<Text>Contamos con procesamiento de pagos, emails, app y más. ¡Te estamos esperando! No te quedes sin jugar 🏃‍♂️</Text>
+							<Text className="text-[15px] text-center">
+								<a href="https://matchpointapp.com" className="text-[#f18f01] underline">
+									Ir a MatchPoint
+								</a>
+							</Text>
+						</Section>
+					</Container>
+				</Body>
+			</Tailwind>
+		</Html>
+	);
+}
+
+export default BottiMailFinal;
